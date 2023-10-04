8bd612e3b31fc7e43d3b4184fef45071a398e339
sasinduSilva <sinharaavishka@gmail.com>
2023-10-04 23:03:58 +0530

    toaster created

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -3,7 +3,7 @@ import * as z from "zod";
 import axios from "axios";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import {
     Form,
@@ -17,6 +17,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
     title: z.string().min(1,{
@@ -25,6 +26,9 @@ const formSchema = z.object({
 })
 
 const CreatePage = () => {
+
+    const router = useRouter();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -34,8 +38,15 @@ const CreatePage = () => {
 
  const {isSubmitting, isValid} = form.formState;   
 
- const onSubmit = (values: z.infer<typeof formSchema>)=>{
-    console.log(values);
+ const onSubmit = async(values: z.infer<typeof formSchema>)=>{
+    try {
+
+        const response = await axios.post("/api/course",values);
+        router.push(`/teacher/courses/${response.data.id}`);
+        
+    } catch {
+        toast.error("something went wrong");
+    }
  }
 
     return ( 
@@ -83,6 +94,12 @@ const CreatePage = () => {
                                 Cancel
                             </Button>
                         </Link>
+                        <Button
+                            type="submit"
+                            disabled={!isValid || isSubmitting}
+                        >
+                            Continue
+                        </Button>
                      </div>
                     </form>
                 </Form>
